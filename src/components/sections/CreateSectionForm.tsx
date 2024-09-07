088cdce422b34631d45063b8862f9b7b7a991335
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-07 15:03:20 +0700

    Add SectionList to CreateSectionForm for reordering section

--- src/components/sections/CreateSectionForm.tsx
+++ src/components/sections/CreateSectionForm.tsx
@@ -10,6 +10,7 @@ import { Course, Section } from '@prisma/client'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 
+import SectionList from './SectionList'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import {
@@ -70,6 +71,18 @@ export default function CreateSectionForm({
 		}
 	}
 
+	const onReorder = async (updateData: { id: string; position: number }[]) => {
+		try {
+			await axios.put(`/api/courses/${course.id}/sections/reorder`, {
+				list: updateData,
+			})
+			toast.success('Sections reordered successfully')
+		} catch (err) {
+			console.log('Failed to reorder sections', err)
+			toast.error('Something went wrong!')
+		}
+	}
+
 	return (
 		<div className='px-10 py-6'>
 			<div className='flex gap-5'>
@@ -81,6 +94,14 @@ export default function CreateSectionForm({
 					</Link>
 				))}
 			</div>
+
+			<SectionList
+				items={course.sections || []}
+				onReorder={onReorder}
+				onEdit={(id) =>
+					router.push(`/instructor/courses/${course.id}/sections/${id}`)
+				}
+			/>
 
 			<h1 className='text-xl font-bold mt-5'>Add New Section</h1>
 
