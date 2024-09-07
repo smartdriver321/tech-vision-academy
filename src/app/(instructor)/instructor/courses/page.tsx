c3d42f2415791f38917fa8eb5bc58d84c46a7f1e
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-07 09:31:02 +0700

    Add DataTable to CoursesPage

--- src/app/(instructor)/instructor/courses/page.tsx
+++ src/app/(instructor)/instructor/courses/page.tsx
@@ -2,20 +2,36 @@ import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import { auth } from '@clerk/nextjs/server'
 
+import { db } from '@/lib/db'
+import { DataTable } from '@/components/custom/DataTable'
 import { Button } from '@/components/ui/button'
+import { columns } from '@/components/courses/Columns'
 
-export default function CoursesPage() {
+export default async function CoursesPage() {
 	const { userId } = auth()
 
 	if (!userId) {
 		return redirect('/sign-in')
 	}
 
+	const courses = await db.course.findMany({
+		where: {
+			instructorId: userId,
+		},
+		orderBy: {
+			createdAt: 'desc',
+		},
+	})
+
 	return (
 		<div className='px-6 py-4'>
 			<Link href='/instructor/create-course'>
 				<Button>Create New Course</Button>
 			</Link>
+
+			<div className='mt-5'>
+				<DataTable columns={columns} data={courses} />
+			</div>
 		</div>
 	)
 }
