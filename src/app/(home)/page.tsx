ab09a75968b2540c48c636c9489cee4e23fa836e
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-08 10:41:34 +0700

    Add CourseCard to HomePage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,29 @@
-export default function HomePage() {
-	return <div>HomePage</div>
+import { db } from '@/lib/db'
+import getCoursesByCategory from '../actions/getCourses'
+import CourseCard from '@/components/courses/CourseCard'
+
+export default async function HomePage() {
+	const categories = await db.category.findMany({
+		orderBy: {
+			name: 'asc',
+		},
+		include: {
+			subCategories: {
+				orderBy: {
+					name: 'asc',
+				},
+			},
+		},
+	})
+	const courses = await getCoursesByCategory(null)
+
+	return (
+		<div className='md:mt-5 md:px-10 xl:px-16 pb-16'>
+			<div className='flex flex-wrap gap-7 justify-center'>
+				{courses.map((course) => (
+					<CourseCard key={course.id} course={course} />
+				))}
+			</div>
+		</div>
+	)
 }
---
+++ src/app/actions/getCourses.ts
@@ -0,0 +1,31 @@
+import { Course } from '@prisma/client'
+
+import { db } from '@/lib/db'
+
+const getCoursesByCategory = async (
+	categoryId: string | null
+): Promise<Course[]> => {
+	const whereClause: any = {
+		...(categoryId ? { categoryId, isPublished: true } : { isPublished: true }),
+	}
+	const courses = await db.course.findMany({
+		where: whereClause,
+		include: {
+			category: true,
+			subCategory: true,
+			level: true,
+			sections: {
+				where: {
+					isPublished: true,
+				},
+			},
+		},
+		orderBy: {
+			createdAt: 'desc',
+		},
+	})
+
+	return courses
+}
+
+export default getCoursesByCategory
